2fbf006d5992530b755fd307052f58ecdb6244e0
Aron Shapialevich <aron.shapialevich@student.ehu.lt>
2025-06-01 11:51:52 +0300

    fix: yml and setupPage

--- utils/setupPage.js
+++ utils/setupPage.js
@@ -4,14 +4,13 @@ const launchPage = async () => {
   const browserName = process.env.BROWSER || "chromium";
   const width = parseInt(process.env.VIEWPORT_WIDTH) || 1920;
   const height = parseInt(process.env.VIEWPORT_HEIGHT) || 1080;
-  
 
   const browserType = browserName === "firefox" ? firefox : chromium;
 
   console.log(`Launching ${browserName} with viewport ${width}x${height}`);
 
   const browser = await browserType.launch({
-    headless: false,
+    headless: process.env.CI ? true : false,
     slowMo: browserName === "firefox" ? 100 : 0,
   });
 
